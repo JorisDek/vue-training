4762ae813c22442b3a0f5613f13eb3e1b6f7393c
Joris Dek <jdek@thirdwave.nl>
2018-02-06 17:57:13 +0100

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                About
+            </div>
+        </div>
+    </div>
+</template>
+
+<!--<script>-->
+<!--export default {-->
+  <!--name: 'about'-->
+<!--}-->
+<!--</script>-->
---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <article class="message" v-for="ticker in tickers">
+                    <div class="message-header">
+                        <p>{{ ticker.symbol }}</p>
+                    </div>
+                    <div class="message-body">
+                        Ask: {{ ticker.ask }}
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+
+export default {
+  data () {
+    return {
+      tickers: []
+    }
+  },
+  created: function () {
+    // ajax request
+    axios.get('https://api.hitbtc.com/api/2/public/ticker')
+      .then(({data}) => this.tickers = data)
+  }
+}
+</script>
